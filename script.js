15f19725ad4e63280aa536c4417ec8e68a772939
antoine <antoine.boisard8@gmail.com>
2025-01-28 17:02:42 +0100

    script submit

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-// alert("ca marche");
-// function getValuesTrip() {
-const searchButton = document
-    .querySelector("#submitButton")
-    .addEventListener("click", function () {
-        const departureInput = document.querySelector("#DepartureInput").value;
-        const arrivalInput = document.querySelector("#arrivalInput").value;
-        const dateInput = document.querySelector("#dateInput").value;
-        console.log(departureInput);
-        console.log(arrivalInput);
-        console.log(dateInput);
+
+
+
+document.querySelector("#submitButton").addEventListener("click", function () {
+    const trip = {
+        departure: document.querySelector("#DepartureInput").value,
+        arrival: document.querySelector("#ArrivalInput").value,
+        date: document.querySelector("#dateInput").value
+    }
+
+
+    fetch('http://localhost:3000/trips', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(trip),
+    })
+    .then(response => response.json())
+        .then(data => {
+            console.log(data)
     });
-// }
+});

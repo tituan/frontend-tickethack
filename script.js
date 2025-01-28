c87c097835536f18888a5d7bf1a7bf3009afea40
fabio <fabio.f@outlook.fr>
2025-01-28 14:02:07 +0100

    js first page

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // alert("ca marche");
+// function getValuesTrip() {
+const searchButton = document
+    .querySelector("#submitButton")
+    .addEventListener("click", function () {
+        const departureInput = document.querySelector("#DepartureInput").value;
+        const arrivalInput = document.querySelector("#arrivalInput").value;
+        const dateInput = document.querySelector("#dateInput").value;
+        console.log(departureInput);
+        console.log(arrivalInput);
+        console.log(dateInput);
+    });
+// }

f76963149dce014e547a3eb8966bdb18eaea23d7
antoine <antoine.boisard8@gmail.com>
2025-01-28 19:24:10 +0100

    front

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 document.querySelector("#submitButton").addEventListener("click", function () {
+    
     const trip = {
         departure: document.querySelector("#DepartureInput").value,
         arrival: document.querySelector("#ArrivalInput").value,
         date: document.querySelector("#dateInput").value,
     };
 
-    fetch("http://localhost:3000/", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(trip),
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            if (data.trips) {
-                for (const element of data.trips) {
-                    document.querySelector("#resultBox").innerHTML += `
-                     <div class="newTrip">
-                    <div class="depShow"><p>${element.departure}</p></div>
-                    <div class="arrShow"><p>${element.arrival}</p></div>
-                    <div class="dateShow"><p>${element.date}</p></div>
-                    <button  type="button" class="buyButton" >Search</button>
-                    </div>
-                    `;
+    if (trip.departure && trip.arrival && trip.date) {
+        
+        fetch("http://localhost:3000/", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(trip),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.result === true) {
+                    for (const element of data.trips) {
+                        // document.querySelector("#messagetBox").css('display:none')
+                        document.querySelector("#resultBox").innerHTML += `
+                         <div class="newTrip">
+                        <div class="depShow"><p>${element.departure}</p></div>
+                        <div class="arrShow"><p>${element.arrival}</p></div>
+                        <div class="dateShow"><p>${element.date}</p></div>
+                        <button  type="button" class="buyButton" >Search</button>
+                        </div>
+                        `;
+                    }
+                    // updateDeleteCityEventListener();
+                } else if ((data.result === false)) {
+                    document.querySelector("#messageBox-img").src ="./images/notfound.png";
+                    document.querySelector(".bookingText").textContent = "No trip found.";
                 }
-                // console.log(data.trips);
-                // updateDeleteCityEventListener();
-            } else if ((data.result = false)) {
-                console.log("kikou");
-            } else {
-                document.querySelector("#messageBox-img").src =
-                    "./images/notfound.png";
-                document
-                    .querySelector("#bookingText")
-                    .textContent("No trip found.");
-            }
-            // console.log(data);
-        });
+            });
+    } else {
+        alert("Empty fields")
+    }
+    
 });

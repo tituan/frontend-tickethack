c67c702fc1723c47f6e36bd32e433af7ccfac851
antoine <antoine.boisard8@gmail.com>
2025-01-28 17:10:54 +0100

    test front after merge

--- script.js
+++ script.js
@@ -1,38 +1,4 @@
 
-// alert("ca marche");
-// function getValuesTrip() {
-const searchButton = document
-    .querySelector("#submitButton")
-    .addEventListener("click", function () {
-        const departureInput = document.querySelector("#DepartureInput").value;
-        const arrivalInput = document.querySelector("#arrivalInput").value;
-        const dateInput = document.querySelector("#dateInput").value;
-        let newTrip = Document.querySelector("#newTrip");
-        
-        const departure = document.querySelector("#depShow")
-        const arrival = document.querySelector("#arrShow")
-        const dateShow = document.querySelector("#dateShow")
-        
-        fetch('http://localhost:3000/trips')
-        .then(response => response.json())
-        .then(data => {
-            if (data.trips) {
-                for (let i = 0; i < data.trips.length; i++) {
-                    document.querySelector('#newTrip').innerHTML += `
-                  <div id="depShow">${}</div>
-                    <div id="arrShow">${}</div>
-                    <div id="dateShow">${}</div>
-                    <button  type="button" id="buyButton" >Search</button>
-                `;
-                }
-                updateDeleteCityEventListener();
-            }
-        });
-
-
-
-
-
 document.querySelector("#submitButton").addEventListener("click", function () {
     const trip = {
         departure: document.querySelector("#DepartureInput").value,
@@ -51,15 +17,14 @@ document.querySelector("#submitButton").addEventListener("click", function () {
             if (data.trips) {
                 for (let i = 0; i < data.trips.length; i++) {
                     document.querySelector('#newTrip').innerHTML += `
-                  <div id="depShow">${}</div>
-                    <div id="arrShow">${}</div>
-                    <div id="dateShow">${}</div>
+                  <div id="depShow"></div>
+                    <div id="arrShow"></div>
+                    <div id="dateShow"></div>
                     <button  type="button" id="buyButton" >Search</button>
                 `;
                 }
                 updateDeleteCityEventListener();
             }
-            console.log(data)
-
+        console.log(data)
     });
 });

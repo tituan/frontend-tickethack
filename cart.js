aabc52506a9c4e944f10a25af2b46d97e26caf57
antoine <antoine.boisard8@gmail.com>
2025-01-29 19:20:45 +0100

    add total and add cross for delete

--- cart.js
+++ cart.js
@@ -1,15 +1,26 @@
-// fetch("http://localhost:3000/carts", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({ tripid: tabId }),
-// })
-
-console.log('toitootototo')
 fetch('http://localhost:3000/carts')
 	.then(response => response.json())
 	.then(data => {
-		console.log(data)
-        // for (const element of data) {
-        //     console.log(element.departure)
-        // }
-});
+		console.log(data.dataTrip)
+        const price = []
+        const total = []
+        for (const element of data.dataTrip) {
+            console.log(element)
+            console.log(element.departure)
+            console.log(element.arrival)
+            console.log(element.date)
+            document.querySelector("#cartContainer").innerHTML += `
+            <div class="newTrip">
+                <div class="depShow"><p>${element.departure}</p></div>
+                  <span>></span>
+                <div class="arrShow"><p>${element.arrival}</p></div>
+                <div class="dateShow"><p>${element.date}</p></div>
+                <div class="priceShow"><p>${element.price}€</p></div>
+                <button type="button" class="buyButton">X</button>
+            </div>
+            `;
+            total.push(element.price)
+        }
+        const euro = total.reduce((acc, current) => acc + current, 0)
+        document.querySelector(".totalPriceEuro").textContent = `Total ${euro} euros`;
+    });
